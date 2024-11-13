05f923b03348583af2a4dab2dc2f7ae63370d9ce
zzarr <azharsyah.curug@gmail.com>
2024-11-13 10:23:19 +0700

    up

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,3 +1,14 @@
 @extends('layout.app')
 @section('content')
+    <div class="page-header">
+        <div class="page-title">
+            <h3>Dashboard</h3>
+        </div>
+        <nav class="breadcrumb-one" aria-label="breadcrumb">
+            <ol class="breadcrumb">
+                <li class="breadcrumb-item"><a href="javascript:void(0);"><i data-feather="grid"></i></a></li>
+
+            </ol>
+        </nav>
+    </div>
 @endsection
